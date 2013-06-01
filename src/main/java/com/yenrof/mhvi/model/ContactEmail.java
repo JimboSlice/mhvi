7f4cdbc4ace3e35d267809d0ff99f2a6ebfac936
Jim Forney <jforn99@gmail.com>
2013-06-01 15:10:51 -0400

    cleaned up misc warnings

--- src/main/java/com/yenrof/mhvi/model/ContactEmail.java
+++ src/main/java/com/yenrof/mhvi/model/ContactEmail.java
@@ -2,7 +2,6 @@ package com.yenrof.mhvi.model;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 //import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 //import javax.persistence.FetchType;
@@ -10,13 +9,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+//import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
+
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
--- src/main/java/com/yenrof/mhvi/model/ContactPhone.java
+++ src/main/java/com/yenrof/mhvi/model/ContactPhone.java
@@ -2,7 +2,7 @@ package com.yenrof.mhvi.model;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
+//import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 //import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,14 +10,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 //import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+//import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
+//import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name = "ContactPhone")
